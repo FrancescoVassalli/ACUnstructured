ac4a3dbd5ef8fb4df2f597626362ff9ba9577ae6
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2025-03-31 10:25:33 -0600

    add api

---
+++ api/README.md
@@ -0,0 +1,15 @@
+# AC Unstructure
+Fast API interface for partitioning pdfs with unstructure
+
+## Setup
+1. Setup your `.env` see steps below
+1. Have docker engine running on your system 
+1. In this directory `docker compose up -d`
+1. Go to `localhost:8000` to access the API
+
+### Env
+Make a file in this directory called .env then write this in it with the env variables
+````
+TODO
+````
+
---
+++ api/extract.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Request, Response
+from unstructured.partition.pdf import partition_pdf
+from unstructured.partition.strategies import PartitionStrategy
+from pydantic import BaseModel
+import logging
+from typing import List, Optional
+logger = logging.getLogger(__name__)
+
+router = APIRouter(
+    prefix="/extract",
+    tags=["extract"],
+    responses={404: {"description": "Not found"}},
+)
+
+class ExtractedElement(BaseModel):
+    id: str
+    is_html: bool
+    text: str
+    page_number: Optional[int] = None
+    file_name: str
+
+@router.post("/extract")
+async def extract(request: Request, name:str)->List[ExtractedElement]:
+    elements = partition_pdf(name, include_page_breaks=True, infer_table_structure=True, strategy=PartitionStrategy.HI_RES)
+    extracted_elements = [0]*len(elements)
+    logger.warning(f"{len(elements)} elements found")
+    for i in range(len(elements)):
+        text = elements[i].text
+        is_html = False
+        page_number = None
+        if elements[i].metadata.text_as_html:
+            text = elements[i].metadata.text_as_html
+            is_html = True
+        if elements[i].metadata.page_number:
+            page_number = elements[i].metadata.page_number
+        extracted_elements[i] = ExtractedElement(
+            id=elements[i].id, 
+            text=text, 
+            page_number=page_number, 
+            file_name=name,
+            is_html=is_html
+        )
+
+    return extracted_elements
+
